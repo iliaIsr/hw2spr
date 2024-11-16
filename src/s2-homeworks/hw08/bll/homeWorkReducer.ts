fdbc01d3a396de8accb77ef63bbfb4f35489f8b5
iliaIsr <ilil.sbl91@gmail.com>
2024-11-16 11:25:41 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,48 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            return [...state].sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name);
+                }
+                if (action.payload === 'down') {
+                    return b.name.localeCompare(a.name);
+                }
+                return 0;
+            });
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter(u=>u.age>action.payload)
         }
         default:
             return state
     }
 }
+
+
+
+//
+// import {UserType} from '../HW8'
+//
+// type ActionType =
+//     | { type: 'sort'; payload: 'up' | 'down' }
+//     | { type: 'check'; payload: number }
+//
+// export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+//     switch (action.type) {
+//         case 'sort': { // by name
+//
+//             return state // need to fix
+//         }
+//         case 'check': {
+//
+//             return state // need to fix
+//         }
+//         default:
+//             return state
+//     }
+// }
