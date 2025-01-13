87d4a287b94eaf2e06285ce6ba20e3c45286326b
iliaIsr <ilil.sbl91@gmail.com>
2025-01-13 13:08:17 +0300

    hw123

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -39,34 +39,34 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 // дописать
-                setText('...всё ок)\n' +
-                    'код 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок)')
+                setInfo('код 200 - обычно означает что скорее всего всё ок)')
 
             })
             .catch((e) => {
                 // дописать
-                if (e.response.status === 400) {
-                    setCode('Ошибка 404')
-                    setImage(error400)
-                    setText('Ты не отправил success в body вообще!\n' +
-                        'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
-                }
-                if(e.response.status === 500) {
+                console.log(e)
+                if (e.response.status === 500) {
                     setCode('Ошибка 500')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText('эмитация ошибки на сервере')
                 }
-                else{
+                if (e.response.status === 400) {
+                    setCode('Ошибка 400')
+                    setImage(error400)
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setText('Ты не отправил success в body вообще!')
+                }
+
+                if (e.code === 'ERR_NETWORK') {
                     setCode('error')
                     setImage(errorUnknown)
-                    setText('Network Error\n' +
-                        'AxiosError')
+                    setText("Network Error")
+                    setInfo('AxiosError')
                 }
 
 
-            })
-            .finally(()=>{//+
-                setInfo('')
             })
 
     }
@@ -82,7 +82,7 @@ const HW13 = () => {
                         onClick={send(true)}
                         xType={'secondary'}
                         // дописать
-                        disabled={info==='...loading'}
+                        disabled={info === '...loading'}
 
                     >
                         Send true
@@ -92,7 +92,7 @@ const HW13 = () => {
                         onClick={send(false)}
                         xType={'secondary'}
                         // дописать
-                        disabled={info==='...loading'}
+                        disabled={info === '...loading'}
                     >
                         Send false
                     </SuperButton>
@@ -101,7 +101,7 @@ const HW13 = () => {
                         onClick={send(undefined)}
                         xType={'secondary'}
                         // дописать
-                        disabled={info==='...loading'}
+                        disabled={info === '...loading'}
 
                     >
                         Send undefined
@@ -111,7 +111,7 @@ const HW13 = () => {
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
                         // дописать
-                        disabled={info==='...loading'}
+                        disabled={info === '...loading'}
 
                     >
                         Send null
